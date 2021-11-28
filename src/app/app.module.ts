2475b896941068434b1844a53a7e47428f2f3a60
jawad <jawadhalbymakary@gmail.com>
2021-11-28 21:24:16 +0200

    working on setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import {  HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ImageListComponent } from './image-list/image-list.component';
+import { ImageService } from './shared/images.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [ImageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/image-list/image-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {ImageService} from '../shared/images.service';
+@Component({
+  selector: 'app-image-list',
+  templateUrl: './image-list.component.html',
+  styleUrls: ['./image-list.component.css']
+})
+export class ImageListComponent implements OnInit {
+  images:any[]=[]
+  constructor(private _imageService:ImageService ) { }
+ 
+  ngOnInit(): void {
+  }
+
+}
