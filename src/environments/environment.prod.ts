2b890a9c56a537511072430f1bcb115876bb9ef9
jawad <jawadhalbymakary@gmail.com>
2021-11-29 21:19:06 +0200

    added distribution folder and env prodMode fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  PIXABAY_API_KEY: '24562869-667d384737f9d219418259d73',
+  PIXABAY_API_URL: "https://pixabay.com/api/?key=",
 };
